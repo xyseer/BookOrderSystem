f7125ed16c62f30fc528a531d08b8c16a36ce8f6
xyseer <xyseer2@gmail.com>
2022-04-26 23:07:37 +0800

    add all search function in dbtool

--- BookOrder/DBtools.swift
+++ BookOrder/DBtools.swift
@@ -14,7 +14,7 @@ class DBtools{
     }
     func initDB()->Bool{
         if (searchBySQL("select * from sqlite_master where type='table'").isEmpty){
-            return creatTable(sql: "CREATE TABLE if not exists userTable(userid integer primary key autoincrement,username text not null,password text not null)") && creatTable(sql: "CREATE TABLE if not exists bookTable(bookid integer primary key autoincrement, bookname text not null, bookthumbpath text,bookprice double)") && creatTable(sql: "CREATE TABLE if not exists bookDetailsTable(bookid integer primary key autoincrement, bookimgpath text, bookfixprice double, bookfullname text, bookauthor text, bookISBN text, bookdetailtext text)") && creatTable(sql: "CREATE TABLE if not exists cartTable(userid integer, bookid integer, bookprice double)") && creatTable(sql: "CREATE TABLE if not exists historyTable(userid integer, bookid integer, bookprice double, ordertime text)")}
+            return creatTable(sql: "CREATE TABLE if not exists userTable(userid integer primary key autoincrement,username text not null,password text not null)") && creatTable(sql: "CREATE TABLE if not exists bookTable(bookid integer primary key autoincrement, bookname text not null, bookthumbpath text,bookprice double not null, bookcategory text)") && creatTable(sql: "CREATE TABLE if not exists bookDetailsTable(bookid integer primary key autoincrement, bookimgpath text, bookfixprice double, bookfullname text, bookauthor text, bookISBN text, bookdetailtext text)") && creatTable(sql: "CREATE TABLE if not exists cartTable(userid integer not null, bookid integer not null, bookprice double not null)") && creatTable(sql: "CREATE TABLE if not exists historyTable(userid integer not null, bookid integer, bookprice double, ordertime text)")}
         else {return true}
         
     }
@@ -36,16 +36,11 @@ class DBtools{
     }
     
     func execNoneQuery(_ sql:String)->Bool{
-        if (dbManager.openDB()){
         var sql_t=sql
         if (sql.last != ";"){sql_t=sql+";"}
-            let result=dbManager.execNoneQuerySQL(sql: sql_t)
-            dbManager.closeDB()
-            return result
-        }
-        else{return false}
+        return creatTable(sql: sql_t)
     }
-    //MARK:1 SEARCH
+    // MARK:1 SEARCH
     func searchBySQL(_ sql:String)->[[String:AnyObject]]{
         if (dbManager.openDB()){
         var sql_t=sql
@@ -59,12 +54,32 @@ class DBtools{
     
     func searchUserTable(userid:Int = -1,username:String="")->[[String:AnyObject]]{
         return searchBySQL("SELECT * FROM 'userTable' WHERE username='\(username)' or userid=\(userid)")
-        
+    }
+    
+    func searchBookTable(bookid:Int = -1,bookname:String="",bookcategory:String="")->[[String:AnyObject]]{
+        if (bookcategory != "")
+        {return searchBySQL("SELECT * FROM 'bookTable' WHERE bookname='\(bookname)' or userid=\(bookid) or bookcategory='\(bookcategory)'")}
+        else{return searchBySQL("SELECT * FROM 'bookTable' WHERE bookname='\(bookname)' or userid=\(bookid)")}
+    }
+    
+    func searchBookDetailsTable(bookid:Int = -1,bookfullname:String="")->[[String:AnyObject]]{
+        return searchBySQL("SELECT * FROM 'bookDetailsTable' WHERE bookfullname='\(bookfullname)' or bookid=\(bookid)")
+    }
+    
+    func searchCartTable(userid:Int)->[[String:AnyObject]]{
+        return searchBySQL("SELECT * FROM 'cartTable' WHERE userid=\(userid)")
+    }
+    
+    func searchHistoryTable(userid:Int)->[[String:AnyObject]]{
+        return searchBySQL("SELECT * FROM 'historyTable' WHERE userid=\(userid)")
     }
     
     
+    // MARK:2 WRITE
     
-    
+    func writeUserTable(userid:Int,username:String,password:String)->Bool{
+        return execNoneQuery("INSERT INTO userTable values(\(userid),'\(username)','\(password)')")
+    }
     
     
     
